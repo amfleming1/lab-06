0cf79212e1ebba6eafd27ec62deeb866c3613b74
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-05-04 02:55:30 +0000

    added html and js pages

---
+++ js/lemon-glazed.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let salePrice = 6.00;
+
+let saleTax = 5.2;
+
+let discount1 = 15;
+
+let discount2 = 5;
+
+let shippingCost = 3.99
+
+if (total> 25 ) {
+    console.log(total);
+
+}
+
