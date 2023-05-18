c6e594da7c1be37af4e9904070f07e9c9b2fc4ac
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-05-18 02:35:59 +0000

    made changes to lab 6x

---
+++ js/app.js
@@ -0,0 +1,263 @@
+console.log('Hello World')
+let parentForSales = document.getElementById("sales-data"); 
+console.log(parentForSales)
+let salesdataElement = document.createElement("p");
+console.log(salesdataElement)
+salesdataElement.textContent = 'Sales Info'
+console.log(salesdataElement)
+parentForSales.appendChild(salesdataElement);
+
+let Hours1 = ['8am', '9am', '10am', '11am', '12pm']
+let customer = 0
+
+let Location1 = 'Seattle'
+let min1 = 23;
+let max1 = 65;
+let avg1 = 6.3;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+let Seattle8am = getRandomInt(23, 65);
+let Total8am = 6.3 * Seattle8am;
+
+console.log("getRandomInt");
+console.log(Seattle8am);
+console.log(total8am);
+
+let Seattle9am = getRandomInt(23, 65);
+let Total9Am = 6.3 * Seattle9am;
+
+console.log("getRandomInt");
+console.log(Seattle9am);
+console.log(Total9am);
+
+let Seattle10am = getRandomInt(23, 65);
+let Total10Am = 6.3 * Seattle10am;
+
+console.log("getRandomInt");
+console.log(Seattle10am);
+console.log(Total0am);
+
+let Seattle11am = getRandomInt(23, 65);
+let total11Pm = 6.3 * Seattle11am;
+
+console.log("getRandomInt");
+console.log(Seattle11am);
+console.log(Total11am);
+
+let Seattle12pm = getRandomInt(23, 65);
+let Total12Pm = 6.3 * Seattle12pm;
+
+console.log("getRandomInt");
+console.log(Seattle12pm);
+console.log(Total2pm);
+
+let hours2 = ['8am', '9am', '10am', '11am', '12pm']
+let Customers = 0
+
+let location2 = 'Tokyo'
+let min2 = 3;
+let max2 = 24;
+let avg2 = 1.2;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+let Tokyo8am = getRandomInt(min2, max2);
+let total8Am = total2 * Tokyo8am;
+
+for (let i = 0; i < 4; i++) {
+
+console.log("getRandomInt");
+console.log(Tokyo8am);
+console.log(Total8am);
+}
+
+let Tokyo9am = getRandomInt(3, 24);
+let total9Am = 1.2 * Tokyo9am;
+
+console.log("getRandomInt");
+console.log(Tokyo9am);
+console.log(Total9am);
+
+let Tokyo10am = getRandomInt(3, 24);
+let total10Am = 1.2 * Tokyo10am;
+
+console.log("getRandomInt");
+console.log(Tokyo10am);
+console.log(Total0am);
+
+let Tokyo11am = getRandomInt(3, 24);
+let total11Am = 1.2 * Seattle11am;
+
+console.log("getRandomInt");
+console.log(Tokyo11am);
+console.log(Total11am);
+
+let Tokyo12pm = getRandomInt(3, 24);
+let total12Pm = 1.2 * Tokyo12pm;
+
+console.log("getRandomInt");
+console.log(Tokyo12pm);
+console.log(Total2pm);
+
+let hours3 = ['8am', '9am', '10am', '11am', '12pm']
+let customers3 = 0
+
+let location3 = 'Dubai'
+let min3 = 11;
+let max3 = 38;
+let avg3 = 3.7;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+let Dubai8am = getRandomInt(11, 38);
+let total8Am = 3.7 * Dubai8am;
+
+for (let i = 0; i < 4; i++){
+console.log("getRandomInt");
+console.log(Dubai8am);
+console.log(Total8am);
+}
+
+let Dubai9am = getRandomInt(11, 38);
+let total9am = 3.7 * Dubai9am;
+
+console.log("getRandomInt");
+console.log(Dubai9am);
+console.log(Total9am);
+
+let Dubai10am = getRandomInt(11, 38);
+let Total10Am = 3.7 * Dubai10am;
+
+console.log("getRandomInt");
+console.log(Dubai10am);
+console.log(Total0am);
+
+let Dubai11am = getRandomInt(11, 38);
+let total11am = 3.7 * Dubai11am;
+
+console.log("getRandomInt");
+console.log(Dubai11am);
+console.log(Total11am);
+
+let Dubai12pm = getRandomInt(11, 38);
+let total12pm = 3.7 * Dubai12pm;
+
+console.log("getRandomInt");
+console.log(Dubai12pm);
+console.log(Total2pm);
+
+let hours4 = ['8am', '9am', '10am', '11am', '12pm']
+let customers4 = 0
+
+let location4 = 'Paris'
+let min4 = 20;
+let max4 = 38;
+let avg4 = 2.3;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+let Paris8am = getRandomInt(20, 38);
+let total8am = 2.3 * Paris8am;
+
+for (let i = 0; i < 4; i++) {
+console.log("getRandomInt");
+console.log(Paris8am);
+console.log(Total8am);
+}
+
+let Paris9am = getRandomInt(20, 38);
+let total9am = 2.3 * Paris9am;
+
+console.log("getRandomInt");
+console.log(Paris9am);
+console.log(Total9am);
+
+let Paris10am = getRandomInt(20, 38);
+let total10am = 2.3 * Paris10am;
+
+console.log("getRandomInt");
+console.log(Paris10am);
+console.log(Total0am);
+
+let Paris11am = getRandomInt(20, 38);
+let total11am = 2.3 * Paris11am;
+
+console.log("getRandomInt");
+console.log(Paris11am);
+console.log(total11am);
+
+let Paris12pm = getRandomInt(20, 38);
+let total12pm = 2.3 * Paris12pm;
+
+console.log("getRandomInt");
+console.log(Paris12pm);
+console.log(total12pm);
+
+let hours5 = ['8am', '9am', '10am', '11am', '12pm']
+let customers5 = 0
+
+let location5 = 'Lima'
+let min5 = 2;
+let max5 = 16;
+let avg5 = 4.6;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+let Lima8am = getRandomInt(2, 16);
+let total8am1 = 4.6 * Lima8am;
+
+for (let i = 0; i < 4; i++){
+console.log("getRandomInt");
+console.log(Lima8am);
+console.log(total8am1);
+}
+
+let Lima9am = getRandomInt(2, 16);
+let Total9am = 4.6 * Lima9am;
+
+console.log("getRandomInt");
+console.log(Lima9am);
+console.log(total9am);
+
+let Lima10am = getRandomInt(2, 16);
+let Total10am = 4.6 * Lima10am;
+
+console.log("getRandomInt");
+console.log(Lima10am);
+console.log(Total0am);
+
+let Lima11am = getRandomInt(2, 16);
+let Total11am = 4.6 * Lima11am;
+
+console.log("getRandomInt");
+console.log(Lima11am);
+console.log(Total11am);
+
+let Lima12pm = getRandomInt(2, 16);
+let Total12pm = 4.6 * Lima12pm;
+
+console.log("getRandomInt");
+console.log(Lima12pm);
+console.log(Total2pm);
+
